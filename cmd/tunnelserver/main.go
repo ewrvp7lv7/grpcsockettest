1dda6c1166bc943f18488a0edc28bcc3430f5a7a
Artyom <user@mail.com>
2022-01-24 17:14:08 +0700

    PermitWithoutStream: true,

--- cmd/tunnelserver/main.go
+++ cmd/tunnelserver/main.go
@@ -87,7 +87,7 @@ func main() {
 
 	var kaep = keepalive.EnforcementPolicy{
 		MinTime:             time.Duration(keepalive_ping) * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
-		PermitWithoutStream: false,                                        // Allow pings even when there are no active streams           // send pings even without active streams
+		PermitWithoutStream: true,                                        // Allow pings even when there are no active streams           // send pings even without active streams
 	}
 
 	opts = append(opts, grpc.KeepaliveEnforcementPolicy(kaep))
